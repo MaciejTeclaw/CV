c713122ebb9804b1b729928f38e39d656e3e2daa
Maciej Teclaw <maciejteclaw1990@gmail.com>
2018-08-11 22:41:31 +0200

    dodano pojawianie oraz znikanie elementu

--- js/aboutme.js
+++ js/aboutme.js
@@ -1,11 +1,6 @@
-$(function() {
+$(function () {
 
     const aboutMe = '<div class="about"></div>';
     $('.map').append(aboutMe);
-
-    $('.about').on('click', function (event) {
-        event.preventDefault();
-        alert("Kliknięte")
-    })
 
 });
--- js/movement.js
+++ js/movement.js
@@ -1,35 +1,71 @@
 $(function () {
 
-
     $(document).keydown(function (movement) {
 
-            const player = $('.player');
-            let playerPosition = player.position();
+        const map = $('.map');
+        const info = $('.info');
 
-            // const expPosition = $('.exp').position();
-            // const letterPosition = $('.letter').position();
-            // const skillPosition = $('.skills').position();
-            // const aboutMePosition = $('.aboutMe').position();
-
-            switch (movement.keyCode) {
-                case 37:
-                    player.css('left', playerPosition.left - 10 + 'px');
-                    break;
-                case 38:
-                    player.css('top', playerPosition.top - 10 + 'px');
-                    break;
-                case 39:
-                    player.css('left', playerPosition.left + 10 + 'px');
-                    break;
-                case 40:
-                    player.css('top', playerPosition.top + 10 + 'px');
-                    break;
-            }
+        const about = $('.about');    // ABOUT
+        const aboutPos = about.position();
+        let aboutPosLeft = function () {
+            return (aboutPos.left)
+        };
+        let aboutPosTop = function () {
+            return (aboutPos.top)
+        };
+        const aboutEl = '<div class="aboutEl"></div>'; // ABOUT
+        const addAbout = $('.aboutEl');
 
 
-            //COLLISION
+        const player = $('.player');
+        let playerPosition = player.position();
+        let playerPosLeft = function () {
+            return (playerPosition.left)
+        };
+        let playerPosTop = function () {
+            return (playerPosition.top)
+        };
 
-
+        switch (movement.keyCode) {
+            case 37:
+                player.css('left', playerPosition.left - 10 + 'px');
+                if (playerPosLeft() <= 0) {
+                    player.css('left', playerPosition.left + 10 + 'px')
+                }
+                info.remove();
+                break;
+            case 38:
+                player.css('top', playerPosition.top - 10 + 'px');
+                if (playerPosTop() <= 0) {
+                    player.css('top', playerPosition.top + 10 + 'px')
+                }
+                info.remove();
+                break;
+            case 39:
+                player.css('left', playerPosition.left + 10 + 'px');
+                if (playerPosLeft() >= map.width() - player.width()) {
+                    player.css('left', playerPosition.left - 10 + 'px')
+                }
+                info.remove();
+                break;
+            case 40:
+                player.css('top', playerPosition.top + 10 + 'px');
+                if (playerPosTop() >= map.height() - player.height()) {
+                    player.css('top', playerPosition.top - 10 + 'px')
+                }
+                info.remove();
+                break;
         }
-    )
+
+        if (playerPosLeft() >= aboutPosLeft() && playerPosTop() >= aboutPosTop()) {
+            if (addAbout.length === 0){
+                map.append(aboutEl)
+            }
+        }else {
+            addAbout.remove()
+        }
+
+    });
+
+
 });
