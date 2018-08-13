971285d5b810f0a2c5987038ee7df64034cfb860
Maciej Teclaw <maciejteclaw1990@gmail.com>
2018-08-13 09:04:29 +0200

    dodano wyświetlanie wszystkich elementów

--- js/movement.js
+++ js/movement.js
@@ -29,7 +29,7 @@ $(function () {
         const aboutEl = '<div class="aboutEl border-3">' +
             '<h3>Imię i nazwisko: Maciej Tecław</h3>' +
             '<p>Data urodzenia: 08.03.1990</p>' +
-            '<p>Adres: ul. Jasna Rola, 36F/23, 61-680 Poznań</p>' +
+            '<p>Adres: ul. Jasna Rola, 36F/23, 61-609 Poznań</p>' +
             '<p>Telefon: 669258394</p>' +
             '<p>Email: maciejteclaw1990@gmail.com</p>' +
             '<p>Linki: <br> GitHub: https://github.com/MaciejTeclaw <br>LinkedIn: www.linkedin.com/in/maciej-tecław-388091167/</p>' +
@@ -46,43 +46,71 @@ $(function () {
         let letterPosTop = function () {
             return (letterPos.top)
         };
-        const letterEl = '<div class="letterEl border-3"></div>';
-        const addLetter = $('.letterEl')
+        const letterEl = '<div class="letterEl border-6"></div>';
+        const addLetter = $('.letterEl');
+
+        /* SKILLS AND HOBBY */
+
+        const sah = $('.skillsAndHobby');
+        const sahPos = sah.position();
+        let sahPosLeft = function () {
+            return (sahPos.left)
+        };
+        let sahPosTop = function () {
+            return (sahPos.top)
+        };
+        const sahEl = '<div class="sahEl border-5"></div>';
+        const addSah = $('.sahEl');
+
+        /* EXP */
+
+        const exp = $('.exp');
+        const expPos = exp.position();
+        let expPosLeft = function () {
+            return (expPos.left)
+        };
+        let expPosTop = function () {
+            return (expPos.top)
+        };
+        const expEl = '<div class="expEl border-4"></div>';
+        const addExp = $('.expEl');
 
 
 
         switch (movement.keyCode) {
             case 37:
-                player.css('left', playerPosition.left - 10 + 'px');
+                player.css('left', playerPosition.left - 20 + 'px');
                 if (playerPosLeft() <= 0) {
-                    player.css('left', playerPosition.left + 10 + 'px')
+                    player.css('left', playerPosition.left + 20 + 'px')
                 }
+                console.log(expPosLeft(), expPosTop())
                 info.remove();
                 break;
             case 38:
-                player.css('top', playerPosition.top - 10 + 'px');
+                player.css('top', playerPosition.top - 20 + 'px');
                 if (playerPosTop() <= 0) {
-                    player.css('top', playerPosition.top + 10 + 'px')
+                    player.css('top', playerPosition.top + 20 + 'px')
                 }
                 info.remove();
                 break;
             case 39:
-                player.css('left', playerPosition.left + 10 + 'px');
+                player.css('left', playerPosition.left + 20 + 'px');
                 if (playerPosLeft() >= map.width() - player.width()) {
-                    player.css('left', playerPosition.left - 10 + 'px')
+                    player.css('left', playerPosition.left - 20 + 'px')
                 }
                 info.remove();
                 break;
             case 40:
-                player.css('top', playerPosition.top + 10 + 'px');
+                player.css('top', playerPosition.top + 20 + 'px');
                 if (playerPosTop() >= map.height() - player.height()) {
-                    player.css('top', playerPosition.top - 10 + 'px')
+                    player.css('top', playerPosition.top - 20 + 'px')
                 }
                 info.remove();
                 break;
         }
 
-        if (playerPosLeft() >= aboutPosLeft() && playerPosTop() >= aboutPosTop()) {
+        if (playerPosLeft() >= aboutPosLeft() &&
+            playerPosTop() >= aboutPosTop()) {
             if (addAbout.length === 0) {
                 map.append(aboutEl)
             }
@@ -99,5 +127,22 @@ $(function () {
             addLetter.remove()
         }
 
+        if (playerPosLeft() <= sahPosLeft() + (sah.width() - player.width()) &&
+            playerPosTop() >= sahPosTop()){
+            if(addSah.length === 0){
+                map.append(sahEl)
+            }
+        }else{
+            addSah.remove()
+        }
+
+        if (playerPosLeft() >= expPosLeft() &&
+            playerPosTop() <= expPosTop() + (exp.height() - player.width())){
+            if (addExp.length === 0){
+                map.append(expEl)
+            }
+        }else{
+            addExp.remove()
+        }
     });
 });
