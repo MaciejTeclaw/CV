9e5dfb47c13eed6d26c68b6dad3f40762706f15b
Maciej Teclaw <maciejteclaw1990@gmail.com>
2018-08-21 11:21:42 +0200

    dodano generator cv

---
+++ js/cv.js
@@ -0,0 +1,12 @@
+$(function () {
+
+    const cv = $('.gen');
+
+    cv.on('mouseenter', function () {
+        $('.gen').addClass('cvGenHover')
+    });
+    cv.on('mouseleave', function () {
+        $('.gen').removeClass('cvGenHover')
+    });
+
+});
