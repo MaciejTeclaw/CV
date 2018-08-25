8702d4188796ee5c1a58324db2e24272e43f50ff
Maciej Teclaw <maciejteclaw1990@gmail.com>
2018-08-25 14:40:52 +0200

    dodano prosty plik json zawierający dane do wyświetlenia oraz generowania cv

--- js/movement.js
+++ js/movement.js
@@ -1,209 +1,209 @@
-$(function () {
+// import data from './db/db.js';
 
-    $('.menu').hide();
-    $('.hamburger').on('click', function() {
-        $('.menu').slideToggle();
-    });
+$(function () {
 
     $(document).keydown(function (movement) {
 
-        const map = $('.map');
-        const info = $('.info');
-        const bubbly = $('.arrow_box');
+            const map = $('.map');
+            const info = $('.info');
+            const bubbly = $('.arrow_box');
 
-        /* PLAYER */
+            /* PLAYER */
 
-        const player = $('.player');
-        let playerPosition = player.position();
-        let playerPosLeft = function () {
-            return (playerPosition.left)
-        };
-        let playerPosTop = function () {
-            return (playerPosition.top)
-        };
+            const player = $('.player');
+            let playerPosition = player.position();
+            let playerPosLeft = function () {
+                return (playerPosition.left)
+            };
+            let playerPosTop = function () {
+                return (playerPosition.top)
+            };
 
-        /* ABOUT ME */
+            /* ABOUT ME */
 
-        const about = $('.about');
-        const aboutPos = about.position();
-        let aboutPosLeft = function () {
-            return (aboutPos.left)
-        };
-        let aboutPosTop = function () {
-            return (aboutPos.top)
-        };
+            const about = $('.about');
+            const aboutPos = about.position();
+            let aboutPosLeft = function () {
+                return (aboutPos.left)
+            };
+            let aboutPosTop = function () {
+                return (aboutPos.top)
+            };
 
-        const aboutEl = '<div class="aboutEl border-3">' +
-            '<h3>Imię i nazwisko: Maciej Tecław</h3>' +
-            '<p>Data urodzenia: 08.03.1990</p>' +
-            '<p>Adres: ul. Jasna Rola 36F/23 <br> 61-609 Poznań</p>' +
-            '<p>Telefon: 669258394</p>' +
-            '<p>Email: maciejteclaw1990@gmail.com</p>' +
-            '<p>Linki: <br> GitHub: https://github.com/MaciejTeclaw <br>LinkedIn: www.linkedin.com/in/maciej-teclaw-388091167/</p>' +
-            '</div>';
-        const addAbout = $('.aboutEl');
+            const aboutEl = '<div class="aboutEl border-3">' +
+                '<h3>Imię i nazwisko: Maciej Tecław</h3>' +
+                '<p>Data urodzenia: 08.03.1990</p>' +
+                '<p>Adres: ul. Jasna Rola 36F/23 <br> 61-609 Poznań</p>' +
+                '<p>Telefon: 669258394</p>' +
+                '<p>Email: maciejteclaw1990@gmail.com</p>' +
+                '<p>Linki: <br> GitHub: https://github.com/MaciejTeclaw <br>LinkedIn: www.linkedin.com/in/maciej-teclaw-388091167/</p>' +
+                '</div>';
+            const addAbout = $('.aboutEl');
 
-        /* LETTER */
+            /* LETTER */
 
-        const letter = $('.letter');
-        const letterPos = letter.position();
-        let letterPosLeft = function () {
-            return (letterPos.left)
-        };
-        let letterPosTop = function () {
-            return (letterPos.top)
-        };
-        const letterEl = '<div class="letterEl border-6"></div>';
-        const addLetter = $('.letterEl');
+            const letter = $('.letter');
+            const letterPos = letter.position();
+            let letterPosLeft = function () {
+                return (letterPos.left)
+            };
+            let letterPosTop = function () {
+                return (letterPos.top)
+            };
+            const letterEl = '<div class="letterEl border-6"><p>"Moje wcześniejsze doświadczenie zawodowe opiera się głównie na kontakcie z Klientem. Doszedłem jednak do wniosku, że zmiana branży umożliwi mi podjęcie pracy zgodnej z moimi zainteresowaniami, stworzy większe możliwości samorozwoju, a także pozwoli mi podjąć pracę, która się nie nudzi. Postawiłem na programowanie. Od tamtego momentu ukończyłem stacjonarny kurs JavaScript oraz spędzam coraz więcej wolnego na kodowaniu oraz doszkalaniu się w tej dziedzinie."</p></div>';
+            const addLetter = $('.letterEl');
 
-        /* SKILLS AND HOBBY */
+            /* SKILLS AND HOBBY */
 
-        const sah = $('.skillsAndHobby');
-        const sahPos = sah.position();
-        let sahPosLeft = function () {
-            return (sahPos.left)
-        };
-        let sahPosTop = function () {
-            return (sahPos.top)
-        };
-        const sahEl = '<div class="sahEl border-5">' +
-            '<h3> Umiejętności:</h3>' +
-            '<p> HTML,CSS, JavaScript (JQuery, React), Git, Gulp, Webpack <br> Język Angielski - średniozaawansowany' +
-            '<br> Język niemiecki - podstawowy </p>' +
-            '<h3>Zainteresowania:</h3>' +
-            '<p> Muzyka rockowa oraz elektroniczna <br> Tresura psów <br> Piłka nożna (głównie Ekstraklasa oraz Lech Poznań)' +
-            '<br> Jazda rowerem</p></div>';
-        const addSah = $('.sahEl');
+            const sah = $('.skillsAndHobby');
+            const sahPos = sah.position();
+            let sahPosLeft = function () {
+                return (sahPos.left)
+            };
+            let sahPosTop = function () {
+                return (sahPos.top)
+            };
+            const sahEl = '<div class="sahEl border-5">' +
+                '<h3> Umiejętności:</h3>' +
+                '<p> HTML,CSS, JavaScript (JQuery, React), Git, Gulp, Webpack <br> Język Angielski - średniozaawansowany' +
+                '<br> Język niemiecki - podstawowy </p>' +
+                '<h3>Zainteresowania:</h3>' +
+                '<p> Muzyka rockowa oraz elektroniczna <br> Tresura psów <br> Piłka nożna (głównie Ekstraklasa oraz Lech Poznań)' +
+                '<br> Jazda rowerem</p></div>';
+            const addSah = $('.sahEl');
 
-        /* EXP */
+            /* EXP */
 
-        const exp = $('.exp');
-        const expPos = exp.position();
-        let expPosLeft = function () {
-            return (expPos.left)
-        };
-        let expPosTop = function () {
-            return (expPos.top)
-        };
-        const expEl = '<div class="expEl border-4">' +
-            '<h3> Doświadczenie:</h3>' +
-            '<p>01.2012 - 03.2017 - Pizza Express <br> Kierowca, Dyspozytor-Kierownik Zmiany</p>' +
-            '<p>10.2016 - 04.2018 - OLX Group <br> Specjalista Do Spraw Obsługi Użytkownika</p>' +
-            '<p>08.2018 - chwila obecna - PolPol.pl <br> Stażysta - HTML, CSS, JavaScript (JQuery)</p>' +
-            '</div>';
-        const addExp = $('.expEl');
+            const exp = $('.exp');
+            const expPos = exp.position();
+            let expPosLeft = function () {
+                return (expPos.left)
+            };
+            let expPosTop = function () {
+                return (expPos.top)
+            };
+            const expEl = '<div class="expEl border-4">' +
+                '<h3> Doświadczenie:</h3>' +
+                '<p>01.2012 - 03.2017 - Pizza Express <br> Kierowca, Dyspozytor-Kierownik Zmiany</p>' +
+                '<p>10.2016 - 04.2018 - OLX Group <br> Specjalista Do Spraw Obsługi Użytkownika</p>' +
+                '<p>08.2018 - chwila obecna - PolPol.pl <br> Stażysta - HTML, CSS, JavaScript (JQuery)</p>' +
+                '</div>';
+            const addExp = $('.expEl');
 
-        /* CV */
 
-        const doc = new jsPDF();
-        const specialElementHandlers = {
-            '#editor': function (element, renderer) {
-                return true;
+            switch (movement.keyCode) {
+                case 37:
+                    player.css('left', playerPosition.left - 20 + 'px');
+                    if (playerPosLeft() <= 0) {
+                        player.css('left', playerPosition.left + 20 + 'px')
+                    }
+                    info.fadeOut(300, function () {
+                        info.remove();
+                    });
+                    bubbly.fadeOut(300, function () {
+                        bubbly.remove();
+                    });
+                    break;
+                case 38:
+                    player.css('top', playerPosition.top - 20 + 'px');
+                    if (playerPosTop() <= 0) {
+                        player.css('top', playerPosition.top + 20 + 'px')
+                    }
+                    info.fadeOut(300, function () {
+                        info.remove();
+                    });
+                    bubbly.fadeOut(300, function () {
+                        bubbly.remove();
+                    });
+                    break;
+                case 39:
+                    player.css('left', playerPosition.left + 20 + 'px');
+                    if (playerPosLeft() >= map.width() - player.width()) {
+                        player.css('left', playerPosition.left - 20 + 'px')
+                    }
+                    info.fadeOut(300, function () {
+                        info.remove();
+                    });
+                    bubbly.fadeOut(300, function () {
+                        bubbly.remove();
+                    });
+                    break;
+                case 40:
+                    player.css('top', playerPosition.top + 20 + 'px');
+                    if (playerPosTop() >= map.height() - player.height()) {
+                        player.css('top', playerPosition.top - 20 + 'px')
+                    }
+                    info.fadeOut(300, function () {
+                        info.remove();
+                    });
+                    bubbly.fadeOut(300, function () {
+                        bubbly.remove();
+                    });
+                    break;
             }
-        };
 
-
-        switch (movement.keyCode) {
-            case 37:
-                player.css('left', playerPosition.left - 20 + 'px');
-                if (playerPosLeft() <= 0) {
-                    player.css('left', playerPosition.left + 20 + 'px')
+            if (playerPosLeft() >= aboutPosLeft() &&
+                playerPosTop() >= aboutPosTop()) {
+                if (addAbout.length === 0) {
+                    $(aboutEl).hide().appendTo(map).slideToggle();
                 }
-                info.fadeOut(300, function () {
-                    info.remove();
+            } else {
+                addAbout.slideToggle(500, function () {
+                    $(this).remove();
                 });
-                bubbly.fadeOut(300, function () {
-                    bubbly.remove();
-                });
-                break;
-            case 38:
-                player.css('top', playerPosition.top - 20 + 'px');
-                if (playerPosTop() <= 0) {
-                    player.css('top', playerPosition.top + 20 + 'px')
+            }
+
+            if (playerPosLeft() <= letterPosLeft() + (letter.width() - player.width()) &&
+                playerPosTop() <= letterPosTop() + (letter.height() - player.height())) {
+                if (addLetter.length === 0) {
+                    $(letterEl).hide().appendTo(map).slideToggle();
                 }
-                info.fadeOut(300, function () {
-                    info.remove();
+            } else {
+                addLetter.slideToggle(500, function () {
+                    $(this).remove();
                 });
-                bubbly.fadeOut(300, function () {
-                    bubbly.remove();
-                });
-                break;
-            case 39:
-                player.css('left', playerPosition.left + 20 + 'px');
-                if (playerPosLeft() >= map.width() - player.width()) {
-                    player.css('left', playerPosition.left - 20 + 'px')
+            }
+
+            // function addData() {
+            //     if (playerPosLeft() <= letterPosLeft() + (letter.width() - player.width()) &&
+            //         playerPosTop() <= letterPosTop() + (letter.height() - player.height())) {
+            //         $.getJSON("./db/data.json", function (data) {
+            //             const dupa = "";
+            //             $.each(data, function (letter) {
+            //                 dupa.push("<div class='\"letterEl border-6\"'>" + letter + "</div>")
+            //             });
+            //
+            //             $(dupa).appendTo(map)
+            //         })
+            //
+            //     } else {
+            //         addLetter.slideToggle(500, function () {
+            //             $(this).remove();
+            //         });
+            //     }
+            // }
+
+            if (playerPosLeft() <= sahPosLeft() + (sah.width() - player.width()) &&
+                playerPosTop() >= sahPosTop()) {
+                if (addSah.length === 0) {
+                    $(sahEl).hide().appendTo(map).slideToggle();
                 }
-                info.fadeOut(300, function () {
-                    info.remove();
+            } else {
+                addSah.slideToggle(500, function () {
+                    $(this).remove();
                 });
-                bubbly.fadeOut(300, function () {
-                    bubbly.remove();
-                });
-                break;
-            case 40:
-                player.css('top', playerPosition.top + 20 + 'px');
-                if (playerPosTop() >= map.height() - player.height()) {
-                    player.css('top', playerPosition.top - 20 + 'px')
+            }
+
+            if (playerPosLeft() >= expPosLeft() &&
+                playerPosTop() <= expPosTop() + (exp.height() - player.width())) {
+                if (addExp.length === 0) {
+                    $(expEl).hide().appendTo(map).slideToggle();
                 }
-                info.fadeOut(300, function () {
-                    info.remove();
+            } else {
+                addExp.slideToggle(500, function () {
+                    $(this).remove();
                 });
-                bubbly.fadeOut(300, function () {
-                    bubbly.remove();
-                });
-                break;
-        }
-
-        if (playerPosLeft() >= aboutPosLeft() &&
-            playerPosTop() >= aboutPosTop()) {
-            if (addAbout.length === 0) {
-                $(aboutEl).hide().appendTo(map).slideToggle();
             }
-        } else {
-            addAbout.slideToggle(500, function () {
-                $(this).remove();
-            });
         }
-
-        if (playerPosLeft() <= letterPosLeft() + (letter.width() - player.width()) &&
-            playerPosTop() <= letterPosTop() + (letter.height() - player.height())) {
-            if (addLetter.length === 0) {
-                $(letterEl).hide().appendTo(map).slideToggle();
-            }
-        } else {
-            addLetter.slideToggle(500, function () {
-                $(this).remove();
-            });
-        }
-
-        if (playerPosLeft() <= sahPosLeft() + (sah.width() - player.width()) &&
-            playerPosTop() >= sahPosTop()) {
-            if (addSah.length === 0) {
-                $(sahEl).hide().appendTo(map).slideToggle();
-            }
-        } else {
-            addSah.slideToggle(500, function () {
-                $(this).remove();
-            });
-        }
-
-        if (playerPosLeft() >= expPosLeft() &&
-            playerPosTop() <= expPosTop() + (exp.height() - player.width())) {
-            if (addExp.length === 0) {
-                $(expEl).hide().appendTo(map).slideToggle();
-            }
-        } else {
-            addExp.slideToggle(500, function () {
-                $(this).remove();
-            });
-        }
-
-        $('.gen').click(function () {
-            doc.fromHTML($(' /* wymyślić jak stworzyć pdfa z zmiennych */').html(), 15, 15, {
-                'width': 170,
-                'elementHandlers': specialElementHandlers
-            });
-            doc.save('cvMaciejTeclaw.pdf');
-        });
-    });
+    );
 });
